3496ed05003e40f83cc5fa31e308c73e6f72495c
Joshua Barlow <jalapenojosh@gmail.com>
2024-04-01 16:41:00 -0600

    Added akka console app

---
+++ AkkaCluster/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        await Host.CreateDefaultBuilder(args)
+          .ConfigureServices((hostContext, services) =>
+          {
+              services.AddHostedService<AkkaService>();
+          })
+          .RunConsoleAsync();
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ AkkaCluster/Services/AkkaService.cs
@@ -0,0 +1,55 @@
+using Akka.Actor;
+using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Akka.Actor;
+using Akka.Routing;
+using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class AkkaService : IHostedService, IActorBridge
+{
+    private readonly ActorSystem _actorSystem;
+    private readonly IActorRef _router;
+
+    public AkkaService()
+    {
+        _actorSystem = ActorSystem.Create("MyActorSystem");
+
+        // Create a ConsistentHashingPool router with 5 instances of the MyActor actor
+        _router = _actorSystem.ActorOf(Props.Create<MyActor>().WithRouter(new ConsistentHashingPool(5)), "myRouter");
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        // StartAsync method implementation
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _actorSystem.Terminate();
+        return Task.CompletedTask;
+    }
+
+    public async Task<string> SendMessage(string key, string message)
+    {
+        // Send the message to the router with the given key
+        var response = await _router.Ask<string>(new ConsistentHashableEnvelope(message, key));
+        return response;
+    }
+}
+
+public class MyActor : ReceiveActor
+{
+    private int i = 0;
+    public MyActor()
+    {
+        Receive<string>(message =>
+        {
+            i += 1;
+            Sender.Tell($"Received message: {message} {i} {Self.Path.ToString()}");
+        });
+    }
+}
