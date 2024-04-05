097c3ebf19676b93362d58cbec85c5bba9ee3ceb
Joshua Barlow <jalapenojosh@gmail.com>
2024-04-04 18:14:57 -0600

    Automated commit

--- Asteroids.Shared/Actors/ClientMessages.cs
+++ Asteroids.Shared/Actors/ClientMessages.cs
@@ -1,1 +1,4 @@
 namespace ClientMessages;
+
+public record EnterLobby(Ship UserShip);
+public record CreateClientActor(string Username, string HubConnectionId);
--- Asteroids.Shared/Actors/LobbyMessages.cs
+++ Asteroids.Shared/Actors/LobbyMessages.cs
@@ -8,5 +8,3 @@ public record GetLobbiesResponse(List<string> Lobbies);
 public record LobbyError(string Message);
 public record LobbyDeath(string LobbyName);
 public record ShipUpdate(Ship Updated);
-public record EnterLobby(Ship UserShip);
-public record CreateClientActor(string Username, string HubConnectionId);
