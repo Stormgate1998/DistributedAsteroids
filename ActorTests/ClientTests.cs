b6241cb2ce4ff2c3efe7291a34b061c2d23901c5
Joshua Barlow <jalapenojosh@gmail.com>
2024-04-08 22:09:16 -0600

    finished my tests

--- ActorTests/ClientTests.cs
+++ ActorTests/ClientTests.cs
@@ -130,60 +130,61 @@ public class ClientTests : TestKit
   //   probe.ExpectMsg<JoinLobbyResponse>();
   // }
 
-  // [Fact]
-  // public void JoiningLobbyCreatesShip()
-  // {
-  //   var probe = CreateTestProbe();
-  //   var lobbySupervisor = Sys.ActorOf(Props.Create<LobbySupervisorActor>(), "lobbySupervisor");
-  //   var clientSupervisor = Sys.ActorOf<ClientSupervisorActor>();
+  [Fact]
+  public async void JoiningLobbyCreatesShip()
+  {
+    var (provider, signalRMock) = getServiceProvider();
+    var probe = CreateTestProbe();
+    var lobbySupervisor = Sys.ActorOf(Props.Create<LobbySupervisorActor>(), "lobbySupervisor");
+    var clientSupervisor = Sys.ActorOf(Props.Create(() => new ClientSupervisorActor(provider)), "clientSupervisor");
 
-  //   clientSupervisor.Tell(new CreateClientActor("tony", ""), probe.Ref);
-  //   probe.ExpectMsg<CreateClientActorResponse>();
+    clientSupervisor.Tell(new CreateClientActor("tony", ""));
+    await Task.Delay(100);
+    clientSupervisor.Tell(new CreateLobby("tony"));
+    await Task.Delay(100);
+    clientSupervisor.Tell(new JoinLobby("tony", "tony"), probe.Ref);
+    await Task.Delay(100);
 
-  //   clientSupervisor.Tell(new CreateLobby("tony"), probe.Ref);
-  //   probe.ExpectMsg<CreateLobbyResponse>();
+    lobbySupervisor.Tell(new GetState("tony", "tony"), probe.Ref);
+    var response = probe.ExpectMsg<GameStateSnapshot>();
+    Ship ship = new()
+    {
+      Username = "tony",
+      Direction = 45,
+      Xpos = 50,
+      Ypos = 50,
+      Health = 50,
+      Score = 0,
+    };
+    List<Ship> ships = [ship];
+    response.Game.ships.Should().BeEquivalentTo(ships);
 
-  //   clientSupervisor.Tell(new JoinLobby("tony", "tony"), probe.Ref);
-  //   probe.ExpectMsg<JoinLobbyResponse>();
+  }
 
-  //   lobbySupervisor.Tell(new GetState("tony", "tony"), probe.Ref);
-  //   var response = probe.ExpectMsg<GameStateSnapshot>();
-  //   Ship ship = new("tony")
-  //   {
-  //     Direction = 45,
-  //     Xpos = 50,
-  //     Ypos = 50,
-  //     Health = 50,
-  //     Score = 0,
-  //   };
-  //   List<Ship> ships = [ship];
-  //   response.Game.ships.Should().BeEquivalentTo(ships);
+  [Fact]
+  public async void CreatingClientCanStartGame()
+  {
+    var (provider, signalRMock) = getServiceProvider();
+    var probe = CreateTestProbe();
+    var lobbySupervisor = Sys.ActorOf(Props.Create<LobbySupervisorActor>(), "lobbySupervisor");
+    var clientSupervisor = Sys.ActorOf(Props.Create(() => new ClientSupervisorActor(provider)), "clientSupervisor");
 
-  // }
+    clientSupervisor.Tell(new CreateClientActor("tony", ""));
+    await Task.Delay(100);
 
-  // [Fact]
-  // public void CreatingClientCanStartGame()
-  // {
-  //   var probe = CreateTestProbe();
-  //   var lobbySupervisor = Sys.ActorOf(Props.Create<LobbySupervisorActor>(), "lobbySupervisor");
-  //   var clientSupervisor = Sys.ActorOf<ClientSupervisorActor>();
+    clientSupervisor.Tell(new CreateLobby("tony"));
+    await Task.Delay(100);
 
-  //   clientSupervisor.Tell(new CreateClientActor("tony", ""), probe.Ref);
-  //   probe.ExpectMsg<CreateClientActorResponse>();
+    clientSupervisor.Tell(new JoinLobby("tony", "tony"));
+    await Task.Delay(100);
 
-  //   clientSupervisor.Tell(new CreateLobby("tony"), probe.Ref);
-  //   probe.ExpectMsg<CreateLobbyResponse>();
+    clientSupervisor.Tell(new StartGame("tony"), probe.Ref);
 
-  //   clientSupervisor.Tell(new JoinLobby("tony", "tony"), probe.Ref);
-  //   probe.ExpectMsg<JoinLobbyResponse>();
+    var response = probe.ExpectMsg<GameStateSnapshot>();
 
-  //   clientSupervisor.Tell(new StartGame("tony"), probe.Ref);
+    response.Game.state.Should().Be(GameState.PLAYING);
 
-  //   var response = probe.ExpectMsg<GameStateSnapshot>();
-
-  //   response.Game.state.Should().Be(GameState.PLAYING);
-
-  // }
+  }
 
   //test 2 players join works
 
